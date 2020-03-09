de5461fda40ccdcffa445091ff7cd31ba5afa2b1
reidswan <reidswan@outlook.com>
2020-03-09 13:41:11 +0200

    Add some basic integration tests

---
+++ tests/test_derive.rs
@@ -0,0 +1,45 @@
+use structbuilder_derive::*;
+
+#[derive(StructBuilder, Debug)]
+struct MyTestStruct {
+    name: String,
+    age: usize,
+    weight: Option<usize>,
+    married: Option<bool>,
+}
+
+/**
+ * Simply successfully compiling is a test case
+ */
+#[test]
+fn test_it_compiles() {}
+
+#[test]
+fn test_accessors() {
+    let my_struct = MyTestStruct::new(String::from("Reid"), 25);
+    assert_eq!(my_struct.name(), "Reid");
+    assert_eq!(my_struct.age(), &25);
+    assert_eq!(my_struct.weight(), &None);
+    assert_eq!(my_struct.married(), &None);
+}
+
+#[test]
+fn test_builders() {
+    let my_struct = MyTestStruct::new(String::from("Reid"), 25).with_weight(100);
+    assert_eq!(my_struct.weight(), &Some(100));
+    assert_eq!(my_struct.married(), &None);
+}
+
+#[test]
+fn test_composition_chain() {
+    let my_struct = MyTestStruct::new(String::from("Reid"), 25)
+        .with_weight(100)
+        .with_weight(150)
+        .with_weight(75)
+        .with_married(false)
+        .with_weight(60);
+
+    assert_eq!(my_struct.weight(), &Some(60));
+    assert_eq!(my_struct.married(), &Some(false));
+    
+}
